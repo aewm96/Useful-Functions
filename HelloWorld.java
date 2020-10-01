f3fb0eeada2a6cd30e99a57a97d177e2cbc8c422
aewm96 <56603684+aewm96@users.noreply.github.com>
2020-10-01 13:30:04 +0100

    Hello World - Java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public void helloWorld(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args){
+        HelloWorld printHW = new HelloWorld();
+        printHW.helloWorld();
+    }
+}
